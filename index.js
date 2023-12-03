4b76e9d0feb63034a840270710de44094c532e4c
NitinSingh912 <152010511+NitinSingh912@users.noreply.github.com>
2023-12-03 15:39:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function onClick(){
+    confirm(
+        "You rechecked right "
+    )
+    confirm("you really want to sumbit?")
+    confirm("there is no going back")
+    confirm("alright last time you sure about it?")
+    alert("Form has been submitted")
+}
